a88c1edbefc37be94072529e0a2581782c8ce602
Hussam-Alkdr <hussam@gmail.com>
2024-08-20 05:19:31 -0400

    edites

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const router = express.Router();
-const logger = require('../logger');
 const connectToDatabase = require('../models/db');
-require('dotenv').config();
+const router = express.Router();
+const dotenv = require('dotenv');
+const pino = require('pino');  // Import Pino logger
+dotenv.config();
+
 const mongoCollectionName = process.env.MONGO_COLLECTION_users;
+
+const logger = pino();  // Create a Pino logger instance
+
+//Create JWT secret
 const JWT_SECRET = process.env.JWT_SECRET;
-const saltRounds = 10;
+
 
 router.post('/register', async (req, res) => {
     try {
+        //Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
         const db = await connectToDatabase();
         const collection = db.collection(mongoCollectionName);
 
         const { firstName, lastName, email, password } = req.body;
-
 
         const existingUserByEmail = await collection.findOne({ "email": email });
         const existingUserByName = await collection.findOne({
@@ -29,13 +35,14 @@ router.post('/register', async (req, res) => {
         if (existingUserByName) {
             return res.status(400).send("User already exists with this name.");
         }
-
+        const saltRounds = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, saltRounds);
         const newUser = {
             firstName: firstName,
             lastName:lastName,
             email: email,
-            password: hashedPassword
+            password: hashedPassword,
+            createdAt: new Date(),
         }
 
         await collection.insertOne(newUser);
@@ -51,6 +58,7 @@ router.post('/register', async (req, res) => {
 
         res.status(201).json({ authtoken, email });
     } catch (e) {
+        logger.error(e);
         return res.status(500).send('Internal server error');
     }
 });
